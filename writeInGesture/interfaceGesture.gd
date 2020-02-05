ce7d210e53be23d6687ffbc988c6d9e1fee41979
Guillaume <guillaume2.denis@gmail.com>
2020-02-05 17:34:07 +0100

    scroll on android done

--- writeInGesture/interfaceGesture.gd
+++ writeInGesture/interfaceGesture.gd
@@ -1,10 +1,12 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var swiping = false
+var release = false
+var isswipping = false
+var swipe_start
+var swipe_mouse_start
+var swipe_mouse_times = []
+var swipe_mouse_positions = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,16 +24,72 @@ func _on_Back_pressed():
 
 
 func _on_allbutton_pressed(arg):
-	var description = load("res://interfaceDescriptionGesture.tscn")
-	var root = get_tree().get_root()
-	var current_scene = root.get_child(root.get_child_count() - 1)
-	var button = current_scene.find_node(arg)
-	current_scene = description.instance()
-	get_tree().get_root().add_child(current_scene)
-	current_scene = root.get_child(root.get_child_count() - 1)
-	current_scene.find_node("Name").set_text(button.get_text())
+	if(!release):
+		var description = load("res://interfaceDescriptionGesture.tscn")
+		var root = get_tree().get_root()
+		var current_scene = root.get_child(root.get_child_count() - 1)
+		var button = current_scene.find_node(arg)
+		current_scene = description.instance()
+		get_tree().get_root().add_child(current_scene)
+		current_scene = root.get_child(root.get_child_count() - 1)
+		current_scene.find_node("Name").set_text(button.get_text())
+		
+		var parsed_name = button.get_text().split(" ")
+		current_scene = root.get_child(root.get_child_count() - 1)
+		current_scene.find_node("Sound").set_text(parsed_name[0])
+		current_scene.find_node("Name").set_text(parsed_name[1])
 
-	var parsed_name = button.get_text().split(" ")
-	current_scene = root.get_child(root.get_child_count() - 1)
-	current_scene.find_node("Sound").set_text(parsed_name[0])
-	current_scene.find_node("Name").set_text(parsed_name[1])
+
+# Allows you to scroll a scroll container by dragging.
+# Includes momentum.
+
+func _input(ev):
+	if swiping and ev is InputEventMouseMotion:
+		var delta = ev.position - swipe_mouse_start
+		print(delta.length())
+		if(delta.length()>10):
+			$"MarginContainer/VBoxContainer/ScrollContainer".set_h_scroll(swipe_start.x - delta.x)
+			$"MarginContainer/VBoxContainer/ScrollContainer".set_v_scroll(swipe_start.y - delta.y)
+			swipe_mouse_times.append(OS.get_ticks_msec())
+			swipe_mouse_positions.append(ev.position)
+			isswipping = true
+	elif ev is InputEventMouseButton:
+		if ev.pressed:
+			swiping = true
+			swipe_start = Vector2($"MarginContainer/VBoxContainer/ScrollContainer".get_h_scroll(),
+			 $"MarginContainer/VBoxContainer/ScrollContainer".get_v_scroll())
+			swipe_mouse_start = ev.position
+			swipe_mouse_times = [OS.get_ticks_msec()]
+			swipe_mouse_positions = [swipe_mouse_start]
+		else:
+			#When we release the button
+			swipe_mouse_times.append(OS.get_ticks_msec())
+			swipe_mouse_positions.append(ev.position)
+			var source = Vector2($"MarginContainer/VBoxContainer/ScrollContainer".get_h_scroll(), 
+			$"MarginContainer/VBoxContainer/ScrollContainer".get_v_scroll())
+			var idx = swipe_mouse_times.size() - 1
+			var now = OS.get_ticks_msec()
+			var cutoff = now - 100
+			for i in range(swipe_mouse_times.size() - 1, -1, -1):
+				if swipe_mouse_times[i] >= cutoff: idx = i
+				else: break
+			var flick_start = swipe_mouse_positions[idx]
+			var flick_dur = min(0.3, (ev.position - flick_start).length() / 1000)
+			if flick_dur > 0.0:
+				var tween = Tween.new()
+				add_child(tween)
+				var delta = ev.position - flick_start
+				var target = source - delta * flick_dur * 15.0
+				tween.interpolate_method(self, 'set_h_scroll', source.x, target.x, flick_dur, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+				tween.interpolate_method(self, 'set_v_scroll', source.y, target.y, flick_dur, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+				tween.interpolate_callback(tween, flick_dur, 'queue_free')
+				tween.start()
+			print(isswipping)
+			if isswipping:
+				release = true
+			else:
+				release = false
+			swiping = false
+			isswipping =false
+				
+	

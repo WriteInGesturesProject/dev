af92544fd5b6278fd95634c96350e7c3e4b5e2f2
chabrema <manon.chabre@gmail.com>
2020-02-05 11:16:23 +0100

    merge with master

--- writeInGesture/Global.gd
+++ writeInGesture/Global.gd
@@ -4,7 +4,10 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var level = 1
+var progress1 = 85
+var progress2 = 0
+var progress3 = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():

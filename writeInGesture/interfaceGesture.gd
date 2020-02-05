f9a790f00801f778ee278deae373c51cc94249b7
Guillaume <guillaume2.denis@gmail.com>
2020-02-05 12:05:18 +0100

    genericity in progress

--- writeInGesture/interfaceGesture.gd
+++ writeInGesture/interfaceGesture.gd
@@ -22,18 +22,17 @@ func _on_Back_pressed():
 
 
 
-func _on_pire_pressed():
+func _on_allbutton_pressed(arg):
 	
 	var description = load("res://interfaceDescriptionGesture.tscn")
-	
 	var root = get_tree().get_root()
 	var current_scene = root.get_child(root.get_child_count() - 1)
-	var button = current_scene.find_node("pire")
+	var button = current_scene.find_node(arg)
 	current_scene = description.instance()
 	get_tree().get_root().add_child(current_scene)
-	
 	current_scene = root.get_child(root.get_child_count() - 1)
 	current_scene.find_node("Name").set_text(button.get_text())
 
-	
-	
+
+func _on_agneau_pressed(extra_arg_0):
+	pass # Replace with function body.

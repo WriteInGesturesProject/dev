5a3cb57ed00c3208c2f2430d222ecda7a5ef1aef
uraz <kadirurazalacali@gmail.com>
2020-03-07 16:55:37 +0100

    responsive version

--- writeInGesture/avatarspace.gd
+++ writeInGesture/avatarspace.gd
@@ -1,34 +1,45 @@
 extends Control
 
 var level = 7 #between 1 and 9
-var avatar
 onready var buton = get_node("MarginContainer/VBoxContainer/HBoxContainer4/Button")
 onready var text = get_node("MarginContainer/VBoxContainer/HBoxContainer/Label")
 var current_texture
 var current_avatar
 var selected = false
+var avatars = ["assistant.png","astronaut.png","businessman.png","captain.png","cashier.png","chef.png","concierge.png","cooker.png","courier.png","croupier.png","croupier-1.png","detective.png","disc-jockey.png","engineer.png","gentleman.png","nurse.png","stewardess.png","worker.png"]
 
 func _ready():
-	text.text = "Niveau: "+str(level)
-	for i in range (level*2+1,19):
-		get_node("MarginContainer/VBoxContainer/HBoxContainer"+str(int((i-1)/6) +1)+"/TextureRect" + str(i)).modulate = Color(0.2,0.2,0.2)
+	get_node("Button").rect_position.y = get_viewport().size.y - 90
+	get_node("LineEdit").rect_position.y = get_viewport().size.y - 90
+	get_node("Label").rect_position.y = get_viewport().size.y - 170
+	get_node("Label").text = "Niveau: "+str(level)
+	var avatar_number = 0
+	var column = get_node("MarginContainer/VBoxContainer")
+	for i in range(3):
+		var line = HBoxContainer.new()
+		for k in range(6):
+			var image = TextureRect.new()
+			var control_img = Control.new()
+			image.texture = load("res://art/users/"+avatars[i*6+k])
+			if (i*6+k > level):
+				image.modulate = Color(0.2,0.2,0.2)
+			image.expand = true
+			image.stretch_mode = TextureRect.STRETCH_SCALE_ON_EXPAND
+			image.rect_size.x = get_viewport().size.y / 5
+			image.rect_size.y = get_viewport().size.y / 5
+			image.connect("gui_input",self,"pressed",[image])
+			line.add_constant_override("separation", get_viewport().size.y / 5 +10)
+			control_img.add_child(image)
+			line.add_child(control_img)
+			avatar_number = avatar_number + 1
+		column.add_child(line)
+		column.add_constant_override("separation", get_viewport().size.y/5 + 10)
 	
-#	var content = Global.loadFileInArray("userinfo")
-#	if (content !=null):
-#		buton.visible = true
-#		var textu = load(content[1])
-#		for i in range (1,19):
-#			if (get_node("MarginContainer/VBoxContainer/HBoxContainer"+str(int((i-1)/6) +1)+"/TextureRect" + str(i)) == textu ):
-#				current_avatar = get_node("MarginContainer/VBoxContainer/HBoxContainer"+str(int((i-1)/6) +1)+"/TextureRect" + str(i))
-#				current_texture = current_avatar.texture
-#				current_avatar.texture = check
-#		var name=get_node("MarginContainer/VBoxContainer/LineEdit")
-#		name.text =content[0]
 
 
-func _on_TextureRect_gui_input(event,number):
+
+func pressed(event,avatar):
 	if (event is InputEventMouseButton):
-		avatar = get_node("MarginContainer/VBoxContainer/HBoxContainer"+str(int((number-1)/6) +1)+"/TextureRect" + str(number))
 		if (avatar.modulate != Color(0.2,0.2,0.2)):
 			selected = true
 			if(current_texture !=null):
@@ -37,19 +48,13 @@ func _on_TextureRect_gui_input(event,number):
 			current_avatar = avatar
 			var array = avatar.texture.resource_path.split('/')
 			avatar.texture = load("res://art/users/selected/"+array[4])
+		
 
-
-#test
-func _input(ev):
-	if ev is InputEventKey and ev.scancode == KEY_K:
-		var name="uuu"
-		Global.rewriteFile("userinfo",name+"\n"+str(current_texture.resource_path))
-		get_tree().change_scene("res://home.tscn")
 
 func _on_Button_gui_input(event):
 	if (event is InputEventMouseButton):
 		if (selected):
-			var name=get_node("MarginContainer/VBoxContainer/LineEdit").text
+			var name=get_node("LineEdit").text
 			Global.rewriteFile("userinfo",name+"\n"+str(current_texture.resource_path))
 		get_tree().change_scene("res://home.tscn")
 

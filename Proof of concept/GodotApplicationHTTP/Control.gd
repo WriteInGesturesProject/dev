02f39a4345805e80641e61bba3ea6c7c7654e06e
Gaëtan <gaetan.rival1@etu.univ-grenoble-alpes.fr>
2021-02-24 21:37:26 +0100

    Update Application + form

--- Proof of concept/GodotApplicationHTTP/Control.gd
+++ Proof of concept/GodotApplicationHTTP/Control.gd
@@ -1,21 +1,17 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#Variable
 
+var firstname
+var lastname
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+#GET PARTY
 func _on_Button_Get_pressed():
 	print ("GET HTTP")
 	var query="firstname=gaetan&lastname=ruzafa"
@@ -31,11 +27,17 @@ func _on_HTTPRequestGet_request_completed(result, response_code, headers, body):
 		else:
 			print("HTTP Error")
 
+
+#POST PARTY
 func _on_Button_Post_pressed():
 	print("POST HTTP")
-	var query="firstname=gaetan&lastname=ruzafa"
-	var header= ["From=ArthiphonieTest@gmail.com","Content-Type: applicationArtiphonie","Content-Length: "+str(query.length())]
-	$HTTPRequestPost.request("http://requestbin.net/r/bz4kewhx",header,false,HTTPClient.METHOD_POST,query);
+	if firstname!=null && lastname!=null:
+		var query="firstname="+firstname+"&lastname="+lastname
+		var header= ["From=ArthiphonieTest@gmail.com","Content-Type: applicationArtiphonie","Content-Length: "+str(query.length())]
+		print(query)
+		$HTTPRequestPost.request("http://requestbin.net/r/bz4kewhx",header,false,HTTPClient.METHOD_POST,query);
+	else:
+		print("Error : Fields Null")
 
 
 
@@ -46,3 +48,11 @@ func _on_HTTPRequestPost_request_completed(result, response_code, headers, body)
 			print(body.get_string_from_utf8())
 		else:
 			print("HTTP Error")
+
+func _on_LineEditFirstname_text_changed(new_text):
+	firstname=new_text
+	print(firstname)
+	
+func _on_LineEditLastname_text_changed(new_text):
+	lastname=new_text
+	print(lastname)

6240fa60319b06f607fbc70ff55ba7ba85e1f6ba
Morgan Crociati <morgan.crociati@outlook.com>
2021-02-24 16:29:28 +0100

    big commit (see desc.)
    - Changed the start-up of the application
    - Removed the legacy code of the android permission, as it is now included in Godot
    - Added a new way to change scene and added a loading screen

--- writeInGesture/tools/ManageScreen.gd
+++ writeInGesture/tools/ManageScreen.gd
@@ -11,26 +11,6 @@ var popup
 
 func _ready():
 	pass
-	
-
-func setUp():
-	if(Global.os == "Android") :
-		var permissions = Engine.get_singleton("AndroidPermissions")
-		permissions.init(get_instance_id(), false)
-		if(!permissions.isRecordAudioPermissionGranted()):
-			changeScene("res://tools/ManagePermission.tscn")
-		else:
-			Global.config.setRecordPermission(true)
-			Firstlaunch()
-	else:
-		Firstlaunch()
-
-func Firstlaunch():
-	if(Global.config.getFirstLaunch()):
-		print(Global.config.getFirstLaunch())
-		changeScene("res://page/home/firstLaunch.tscn")
-	changeScene("res://page/home/home.tscn")
-
 
 func screenResearchPopup():
 	print("screenResearch")

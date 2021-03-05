ea9f567e885a7567ae9ac2448062bc6136db6c1e
Claire548 <claire.velut@laposte.net>
2021-03-05 14:45:57 +0100

    Linked goose_game to game_end
    + add score calculation

--- writeInGesture/artiphonie/goose_game/goose_game.gd
+++ writeInGesture/artiphonie/goose_game/goose_game.gd
@@ -6,18 +6,21 @@ var pronouncingRessource := load(Global.artiphonie.PATH_PRONOUNCING)
 var pronouncingLayout: int
 var pronouncingScene: Control
 
-var currentTile = 1
+var currentTile :int = 1
 var tileWords: Array
 
 var maxAttempt: int
 var attempt: int
+var attemptPerWord: Array = []
+
+var difficulty: String
 
 func _ready():
 	var arguments = Global.get_arguments()
-	setup(arguments[0])
-	print(arguments[0])
+	difficulty = arguments[0]
+	setup()
 
-func setup(difficulty: String = "Facile"):
+func setup():
 	match difficulty:
 		"Facile":
 			pronouncingLayout = 1
@@ -32,6 +35,7 @@ func setup(difficulty: String = "Facile"):
 	attempt = maxAttempt
 	tileWords = Global.get_n_word_from_active_list(NB_TILE)
 	move_player_to_next_position()
+	attemptPerWord.resize(NB_TILE)
 	
 
 func get_player_position_on_tile(tileNumber: int) -> Vector2:
@@ -42,7 +46,7 @@ func get_player_position_on_tile(tileNumber: int) -> Vector2:
 func _on_Validate_pressed():
 	find_node("Validate").disabled = true
 	pronouncingScene = pronouncingRessource.instance()
-	pronouncingScene.setup(tileWords[currentTile], pronouncingLayout)
+	pronouncingScene.setup(tileWords[currentTile-1], pronouncingLayout)
 	pronouncingScene.connect("pronounced", self, "_on_pronounced")
 	add_child(pronouncingScene)
 
@@ -52,8 +56,11 @@ func _on_pronounced(word: Word, result: bool):
 		if attempt > 0:
 			return
 	pronouncingScene.queue_free()
+	attemptPerWord[currentTile-1]=maxAttempt-attempt
 	attempt = maxAttempt
 	Global.speechToText.stopListen()
+	if(currentTile == NB_TILE):
+		end_game()
 	currentTile += 1
 	move_player_to_next_position()
 
@@ -64,3 +71,29 @@ func move_player_to_next_position() -> void:
 
 func _on_tween_tween_all_completed():
 	find_node("Validate").disabled = false
+
+func calculate_score():
+	var sum = 0
+	for element in attemptPerWord:
+		sum += element
+	var score = 5 - sum/4 - 1
+	if score<=0:
+		score = 1
+	match difficulty:
+		"Normal":
+			score+=2
+		"Difficile":
+			score+=4
+	return score
+
+
+func end_game():
+	var score = calculate_score()
+	#args to send to game_end
+	var args : Array = []
+	args.append("res://artiphonie/goose_game/goose_game.tscn")
+	args.append("Jeu de l'oie")
+	args.append(difficulty)
+	args.append(score)
+	args.append(14)
+	Global.change_scene("res://shared/game_end/game_end.tscn", args)

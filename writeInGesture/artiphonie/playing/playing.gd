c2e2ea54879c33ac2289c525478d333dbbe0c256
Claire548 <claire.velut@laposte.net>
2021-03-09 16:08:25 +0100

    Add listen and choose in the game menu

--- writeInGesture/artiphonie/playing/playing.gd
+++ writeInGesture/artiphonie/playing/playing.gd
@@ -7,6 +7,12 @@ func _ready():
 	$playing.add_playing_element(Global.artiphonie.GOOSE_GAME_NAME, Global.artiphonie.PATH_GOOSE_GAME, 
 	Global.artiphonie.PATH_GOOSE_GAME_ICON, gooseGameDifficulties)
 	
+	var listenAndChooseDifficulties := []
+	for difficulty in Global.artiphonie.LISTEN_AND_CHOOSE_GAME_DIFFICULTY:
+		listenAndChooseDifficulties.append([difficulty, false, ""])
+	$playing.add_playing_element(Global.artiphonie.LISTEN_AND_CHOOSE_GAME_NAME, Global.artiphonie.PATH_LISTEN_AND_CHOOSE_GAME, 
+	Global.artiphonie.PATH_LISTEN_AND_CHOOSE_GAME_ICON, listenAndChooseDifficulties)
+	
 	var memoryDifficulties := []
 	for difficulty in Global.artiphonie.MEMORY_DIFFICULTY:
 		memoryDifficulties.append([difficulty, false, ""])

a8f9a266aaf192aeaa9d4ae708e1d478b30d51c4
Claire548 <claire.velut@laposte.net>
2021-03-09 17:31:39 +0100

    cleat the code and add comment

--- writeInGesture/artiphonie/listen_choose/card_listen_choose.gd
+++ writeInGesture/artiphonie/listen_choose/card_listen_choose.gd
@@ -4,69 +4,32 @@ class_name CardScene
 
 const Word = preload("res://entity/Word.gd")
 
-var backgroundColor : Panel
-var cardContainer : MarginContainer
-var mainVBox : VBoxContainer
-var imgBorelContainer : VBoxContainer
-var wordLabel : Label
 var word : Word
-var vectorSize : Vector2
-var test : ColorRect
-var controlImageWord : Control
-var selected : bool
-var buttonSelection : Button
-var difficulty = 2
 var currentLayout : Control
 
 signal card_pressed(card)
 
 func _ready():
-#	buttonSelection = Button.new()
-#	backgroundColor = Panel.new()
-#	backgroundColor.set("custom_styles/panel", load("res://assets/theme/backgroundPanel.tres"))
-#	test = ColorRect.new()
-#	test.size_flags_horizontal = SIZE_EXPAND
-#	test.size_flags_vertical = SIZE_EXPAND
-#	test.color = "ffffff"
-#	cardContainer = MarginContainer.new()
-#	mainVBox = VBoxContainer.new()
-#	imgBorelContainer = VBoxContainer.new()
-#	wordLabel = Label.new()
-#	controlImageWord = Control.new()
-#	self.add_child(backgroundColor)
-#	mainVBox.add_child(imgBorelContainer)
-#	mainVBox.add_child(wordLabel)
-#	mainVBox.add_child(controlImageWord)
-#	cardContainer.add_child(test)
-#	mainVBox.size_flags_horizontal = SIZE_EXPAND_FILL
-#	mainVBox.size_flags_vertical = SIZE_EXPAND_FILL
-#	cardContainer.add_child(mainVBox)
-#	self.add_child(cardContainer)
-#	self.add_child(imgBorelContainer)
-#	self.add_child(buttonSelection)
 	pass
 
-func setUpCard(_word : Word, level : int, size : Vector2) -> void:
-
-	currentLayout = find_node("layout_1")
+func setUpCard(_word : Word, difficulty : int, size : Vector2) -> void:
+	#display the right layout depending on the difficulty 
+	currentLayout = find_node("layout_" + String(difficulty))
 	currentLayout.visible = true
-	currentLayout.find_node("word").text = _word.word
+	#set the paramter of the card
 	word = _word
-	
+	currentLayout.find_node("word").text = _word.word
 	currentLayout.find_node("picture").texture = Global.artiphonie.get_word_icon(word.iconPath)
-	print(size)
 	currentLayout.find_node("borel_maisonny_container").rect_min_size.x = size.x*0.75
 	currentLayout.find_node("borel_maisonny_container").rect_min_size.y = size.y/3
-
-	extract_borel_maisonny()
+	#set the card min size
 	self.rect_min_size = size
-	self.rect_size = size
 	
-
+	#add the borel maisonny images to the card
+	extract_borel_maisonny()
+	
 func _on_CardButton_pressed():
-	print("pressss")
 	emit_signal("card_pressed",self)
-	
 	
 #This function extract all the borel maisonny sign picture out of all the
 #phonetic symbol of our function
@@ -78,8 +41,6 @@ func extract_borel_maisonny() -> void:
 #a given container. Before adding the picture, the function resize all the other
 #picture present in the container.
 func add_borel_maisonny(container: HBoxContainer, imgPath: String) -> void:
-	print(container.rect_min_size)
-	print(container.rect_size.x)
 	var newBorelMaisonny = TextureRect.new()
 	newBorelMaisonny.expand = true
 	newBorelMaisonny.stretch_mode = newBorelMaisonny.STRETCH_KEEP_ASPECT_CENTERED
@@ -94,155 +55,3 @@ func add_borel_maisonny(container: HBoxContainer, imgPath: String) -> void:
 		otherBorelMaisonny.rect_min_size.x = correctSize
 		otherBorelMaisonny.rect_min_size.y = correctSize
 	container.add_child(newBorelMaisonny)
-	
-	
-	
-func setImgBorel() -> void :
-#		Catch image borel
-		var phonetic = word.get_phonetic()
-		var arrayPicture = Global.phoneticToArrayPicturePath(phonetic)
-		var line1 : HBoxContainer
-#		More than 3 images borel
-		if(arrayPicture.size() > 3):
-
-			var line2 : HBoxContainer
-			line1 = Global.putBorelInHboxContainer([arrayPicture[0],arrayPicture[1],arrayPicture[2]], imgBorelContainer.rect_min_size.x-2 , imgBorelContainer.rect_min_size.y/2)
-			var tab = []
-			for i in range(3, arrayPicture.size()):
-				tab.append(arrayPicture[i])
-			line2 = Global.putBorelInHboxContainer(tab, imgBorelContainer.rect_min_size.x-2, imgBorelContainer.rect_min_size.y/2)
-			imgBorelContainer.add_child(line1)
-			imgBorelContainer.add_child(line2)
-		else :
-			line1 = (Global.putBorelInHboxContainer(arrayPicture, vectorSize.x, (vectorSize.x)/arrayPicture.size()))
-			imgBorelContainer.add_child(line1)
-
-func _scaleImg(textureRect, size):
-	if textureRect.texture.get_size().x < textureRect.texture.get_size().y :
-		textureRect.rect_size.y = size
-		textureRect.rect_size.x = size * (textureRect.texture.get_size().x / textureRect.texture.get_size().y)
-#	elif textureRect.texture.get_size().x > vectorSize.x && textureRect.texture.get_size().y > size :
-	else:
-		textureRect.rect_size.x = vectorSize.x 
-		textureRect.rect_size.y = textureRect.texture.get_size().y * (vectorSize.x / textureRect.texture.get_size().x)
-		if textureRect.rect_size.y > size :
-			textureRect.rect_size.x *= size / textureRect.rect_size.y
-			textureRect.rect_size.y = size
-#	else :
-		
-
-func mySize(word):
-	var size = 0
-	for i in word:
-		if(i.to_ascii()[0] != 3):
-			size += 1
-	if(size == 1):
-		return 1.7
-	return size
-
-
-
-#extends Control
-#
-#var word: Word
-#var currentLayout: Control
-#
-#signal pronounced(word, result)
-#
-#func _ready():
-#	set_process(false)
-#
-##_word: Word -> The word we want the user to pronounce
-##layout: int -> [1:7] number of the layout we want to use (see below)
-##layout = 1 -> sound + picture + borel maisonny sign
-##layout = 2 -> sound + borel maisonny signs
-##layout = 3 -> sound + picture
-##layout = 4 -> sound
-##layout = 5 -> picture + borel maisonny signs
-##layout = 6 -> picture
-##layout = 7 -> borel maisonny signs
-#func setup(_word: Word, layout: int = 1):
-#	word = _word
-#	currentLayout = find_node("layout_" + String(layout))
-#	currentLayout.visible = true
-#	$background/word.text = word.word
-#	extract_borel_maisonny()
-#	currentLayout.find_node("picture").texture = Global.artiphonie.get_word_icon(word.iconPath)
-#
-##This function extract all the borel maisonny sign picture out of all the
-##phonetic symbol of our function
-#func extract_borel_maisonny() -> void:
-#	for imgPath in Global.artiphonie.phonetic_to_array_picture_path(word.phonetic):
-#		add_borel_maisonny(currentLayout.find_node("borel_maisonny_container"), imgPath)
-#
-##This function add a picture of a phonetic symbole in borel maisonny sign to
-##a given container. Before adding the picture, the function resize all the other
-##picture present in the container.
-#func add_borel_maisonny(container: HBoxContainer, imgPath: String) -> void:
-#	var newBorelMaisonny = TextureRect.new()
-#	newBorelMaisonny.expand = true
-#	newBorelMaisonny.stretch_mode = newBorelMaisonny.STRETCH_KEEP_ASPECT_CENTERED
-#	newBorelMaisonny.texture = load(imgPath)
-#	newBorelMaisonny.size_flags_horizontal = newBorelMaisonny.SIZE_SHRINK_CENTER
-#	var numberOtherBorelMaisonnyInContainer = container.get_child_count() + 1
-#	var maxSizeX = container.rect_min_size.x/numberOtherBorelMaisonnyInContainer
-#	var correctSize = min(maxSizeX, container.rect_min_size.y)
-#	newBorelMaisonny.rect_min_size.x = correctSize
-#	newBorelMaisonny.rect_min_size.y = correctSize
-#	for otherBorelMaisonny in container.get_children():
-#		otherBorelMaisonny.rect_min_size.x = correctSize
-#		otherBorelMaisonny.rect_min_size.y = correctSize
-#	container.add_child(newBorelMaisonny)
-#
-#func _on_listen_pressed():
-#	match OS.get_name():
-#		"Android":
-#			Global.textToSpeech.speakText(word.word)
-#
-#func _on_record_pressed():
-#	match OS.get_name():
-#		"Android":
-#			if not Global.speechToText.isListening():
-#				currentLayout.find_node("record").modulate = Color(1,1,1,0.5)
-#				Global.speechToText.stopListen()
-#				Global.speechToText.doListen()
-#				set_process(true)
-#			else:
-#				currentLayout.find_node("record").modulate = Color(1,1,1,1)
-#				Global.speechToText.stopListen()
-#				set_process(false)
-#
-##The _process function in this scene is used only for the speech to text,
-##basicaly we wait for the user to have said something and when he has said
-##something Global.speechToText.isDetectDone() will return true. Then we analyse
-##what the user said and we do what we have to do if what the user said is right
-##or wrong.
-#func _process(_delta):
-#	match OS.get_name():
-#		"Android":
-#			if Global.speechToText.isError():
-#				currentLayout.find_node("record").modulate = Color(1,1,1,0.5)
-#				Global.speechToText.stopListen()
-#				Global.speechToText.doListen()
-#			if Global.speechToText.isDetectDone():
-#				currentLayout.find_node("record").modulate = Color(1,1,1,1)
-#				var sentence = Global.speechToText.getWords()
-#				set_process(false)
-#				Global.speechToText.stopListen()
-#				if Global.artiphonie.check_words(sentence, word):
-#					$correct.playing = true
-#				else:
-#					$incorrect.playing = true
-#
-#func _on_correct_finished():
-#	#We wait for the sound to finish before signaling of the result
-#	emit_signal("pronounced", word, true)
-#	pass
-#
-#func _on_incorrect_finished():
-#	#We wait for the sound to finish before signaling of the result
-#	emit_signal("pronounced", word, false)
-#	pass
-
-
-
--- writeInGesture/artiphonie/listen_choose/listen_choose.gd
+++ writeInGesture/artiphonie/listen_choose/listen_choose.gd
@@ -2,44 +2,26 @@ extends Control
 
 var CardScene = preload("./card_listen_choose.tscn")
 
-var textToSpeech = Global.textToSpeech
+const NB_SCREEN_MAX = 10
 
+var cardGrid : HBoxContainer
+var sizeScene : Vector2
+var sizeCard : Vector2
 
-const NB_SCREEN = 10
-
-var words = ""
-var display = false
-var incremented = false
-var listOfWords : Array
 var listOfUndoneWords : Array
 
-var index : int = 0
-var img = ""
-var container
-var line1 #line 1 in case of more than 3 borel image
-var line2 #line 2 in case of more than 3 borel image
-var choice #choix fait par l'enfant
-var rand = 0 #choix aléatoire de l'image à trouver
-var prevChoice : ColorRect
-var plate : HBoxContainer
-var sizeScene : Vector2
-var cardSelected : CardScene
-var cardToFind : CardScene
-var indexWord : int
-var score : int = 0
+var cardToFind : Word
+
+var nb_screen : int = 0
+var nbRight : int = 0
 var difficulty : String
-var sizeCard : Vector2
-var currentCard : CardScene
-#var card2 : Card
-#var card3 : Card
 var cardLayout : int
 
 func _ready():
-	
-#	var arguments = Global.get_arguments()
-#	difficulty = arguments[0]
-	difficulty = "Facile"
-	
+
+	var arguments = Global.get_arguments()
+	difficulty = arguments[0]
+
 	match difficulty:
 		"Facile":
 			cardLayout = 1
@@ -47,90 +29,60 @@ func _ready():
 			cardLayout = 2
 		"Difficile":
 			cardLayout = 3
-			
 	
-	listOfWords = Global.activeList.words.duplicate(true)
 	listOfUndoneWords = Global.activeList.words.duplicate(true)
 	
-	plate = find_node("plate")
-	prevChoice = null
+	#set up of the gridCard section
+	cardGrid = find_node("cardGrid")
 	sizeScene = Vector2(self.rect_size.x,self.rect_size.y)
 	sizeCard = Vector2((sizeScene.x - 2*((sizeScene.x*0.25)/6))/3, sizeScene.y*0.6)
-	
-	var speakButton = find_node("speak")
-	
-	var validateButton = find_node("Validate")
-	
 	var yRest = - find_node("speak").rect_size.y - find_node("Validate").rect_size.y - sizeCard.y + sizeScene.y
-#	plate.add_constant_override("separation", sizeCard.x + (sizeScene.x*0.25)/3)
-	find_node("marginPlate").add_constant_override("margin_left", (sizeScene.x*0.25)/6)
-	find_node("marginPlate").add_constant_override("margin_top", yRest/2 + speakButton.rect_size.y)
+	find_node("marginCardGrid").add_constant_override("margin_left", (sizeScene.x*0.25)/6)
+	find_node("marginCardGrid").add_constant_override("margin_top", yRest/2 + find_node("speak").rect_size.y)
 	next_screen()
 	
 func next_screen():
-	if(index < NB_SCREEN && listOfUndoneWords.size() >= 3):
-		var wordToFind = get_n_word_from_given_list(listOfUndoneWords,1)
-		currentCard = CardScene.instance()
-		currentCard.name = String(0)
-		find_node("plate").add_child(currentCard)
-		currentCard.setUpCard(wordToFind[0], cardLayout, sizeCard)
-		currentCard.connect("card_pressed", self, "_on_Validate_pressed")
-		cardToFind = currentCard
-		listOfUndoneWords.erase(wordToFind[0])
-		
-		var wordsToComplete = get_n_word_from_given_list(listOfUndoneWords,2)
-		for i in range (1,3):
-			print("coucou")
+	var currentCard : CardScene
+	#the game continue while we all the screen haven't been done
+	#bt while there are still enough word available in listOfUndoneWords
+	if(nb_screen < NB_SCREEN_MAX && listOfUndoneWords.size() >= 3):
+		var wordsToShow = get_n_word_from_given_list(listOfUndoneWords,3)
+		#for the 3 cards that will be displayed
+		for i in range (0,3):
 			currentCard = CardScene.instance()
 			currentCard.name = String(i)
-			find_node("plate").add_child(currentCard)
-			currentCard.setUpCard(wordsToComplete[i-1], cardLayout, sizeCard)
+			find_node("cardGrid").add_child(currentCard)
+			currentCard.setUpCard(wordsToShow[i], cardLayout, sizeCard)
 			currentCard.connect("card_pressed", self, "_on_Validate_pressed")
-		index +=1
+		#choose randomly the card to find
+		var cardAlea = randi() % 3
+		cardToFind = wordsToShow[cardAlea]
+		#delete the word to find to not choose it again
+		listOfUndoneWords.erase(cardToFind)
+		nb_screen +=1
 	else :
 		game_end()
 
-func _cardSelected(event : InputEvent, card : CardScene) -> void:
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-#		if cardSelected != null :
-#			cardSelected.backgroundColor.set("custom_styles/panel", load("res://assets/theme/backgroundPanelSelected.tres"))
-#		card.backgroundColor.set("custom_styles/panel", load("res://assets/theme/backgroundPanel.tres"))
-		cardSelected = card
-#		_on_Validate_pressed()
-
+#tell the word to find when the button is pressed
 func _on_speak_pressed():
-	if(textToSpeech != null):
-		var text = cardToFind.wordLabel.text
-		match OS.get_name():
-			"X11":
-				textToSpeech.speak(text, false)
-			"Android":
-				textToSpeech.speakText(text)
+	match OS.get_name():
+		"X11":
+			Global.textToSpeech.speak(cardToFind.word, false)
+		"Android":
+			Global.textToSpeech.speakText(cardToFind.word)
 
+#is called when a card is pressed
 func _on_Validate_pressed(card):
-	if(cardToFind.find_node("layout_" + String(cardLayout)).find_node("word").text == card.word.word):
-		print("score")
-		score += 1
+	if(cardToFind.word == card.word.word):
+		nbRight += 1
 		find_node("Good").playing = true
 	else : 
 		find_node("Wrong").playing = true
-	cardSelected = null
-	for i in range(0, plate.get_child_count()):
-		plate.get_child(i).queue_free()
+	for i in range(0, cardGrid.get_child_count()):
+		cardGrid.get_child(i).queue_free()
 	next_screen()
 
-
-func _on_speakControl_gui_input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-		if(textToSpeech != null):
-			var text = cardToFind.wordLabel.text
-			print(text)
-			match OS.get_name():
-				"X11":
-					textToSpeech.speak(text, false)
-				"Android":
-					textToSpeech.speakText(text)
-
+#return n word from the fiven list
 func get_n_word_from_given_list(list : Array , n: int) -> Array:
 	var randomPositions: Array = []
 	randomPositions.append(randi() % list.size())
@@ -144,9 +96,11 @@ func get_n_word_from_given_list(list : Array , n: int) -> Array:
 		result.append(list[randomPosition])
 	return result
 
-
 func calculate_score():
-	return 2
+	var score = (5 * nbRight)/nb_screen
+	if score == 0:
+		score+=1
+	return score
 	
 func game_end():
 	var score = calculate_score()

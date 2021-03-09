a5983efb4aee2eb6214800922fd5264001850ad6
Morgan Crociati <morgan.crociati@outlook.com>
2021-03-09 17:27:49 +0100

    Added a fake/stub database to handle basic communication

---
+++ writeInGesture/management/database.gd
@@ -0,0 +1,18 @@
+extends Node
+
+class_name DataBase
+
+const LOCAL_PATH := "res://database/"
+const LOCAL := true
+
+func get_word(word: String) -> Word:
+	return null
+
+func get_words(listName: String) -> Words:
+	return null
+
+func get_player(playerName: String) -> Player:
+	return null
+
+func get_icon(iconName: String) -> Texture:
+	return null

515a0fa21d34d567fd9733a755da024030030a62
Claire548 <claire.velut@laposte.net>
2021-03-12 10:42:43 +0100

    fix the path name
    The avatar_space scene changed name so the avatar button need to launch the new name of the scene

--- writeInGesture/shared/avatarspace/avatar_button.gd
+++ writeInGesture/shared/avatarspace/avatar_button.gd
@@ -4,4 +4,4 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_avatar_button_pressed():
-	Global.change_scene("res://shared/avatarspace/avatarSpace1.tscn")
+	Global.change_scene("res://shared/avatarspace/avatar_space.tscn")

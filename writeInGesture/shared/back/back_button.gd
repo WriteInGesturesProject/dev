0168f632e06b86c206c804a60dfb368b49b9ab93
Morgan Crociati <morgan.crociati@outlook.com>
2021-03-17 10:34:47 +0100

    commented back btn

--- writeInGesture/shared/back/back_button.gd
+++ writeInGesture/shared/back/back_button.gd
@@ -1,13 +1,20 @@
 extends Button
 
+# The back button is one the scene that is used pretty much everywhere.
+# The back button as 3 states:
+# back: Return to the previous scene (which is the scene the player previously was on)
+#		so this is usefull when we want to automate the process which is most of the
+#		however it does not work when we don't want the player to go back to the previous
+#		scene, for example in a game when the player go to the end game scene we want him
+#		to go back to the home page which is not the previous scene
+# home: Return to the home scene of the current app
+# custom: Here you specify the path and the args given and it could go anywhere
+
 enum REDIRECTION {back, home, custom}
 
 export (REDIRECTION) var redirection = REDIRECTION.back
 export var customPath : String = ""
 export var customArgs : Array = []
-
-func _ready():
-	pass # Replace with function body.
 
 func back_button_pressed():
 	match redirection:

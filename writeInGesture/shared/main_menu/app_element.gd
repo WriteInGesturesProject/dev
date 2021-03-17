15225a157ea8f3b7b82987bd522f2918cad059e3
Morgan Crociati <morgan.crociati@outlook.com>
2021-03-17 13:09:43 +0100

    playing commented

--- writeInGesture/shared/main_menu/app_element.gd
+++ writeInGesture/shared/main_menu/app_element.gd
@@ -1,12 +1,21 @@
 extends Control
 
+# App element is a brick for the main menu
+# It represent the icon of the application
+
 var appScenePath: String
 var appName: String
 
-func setUp( _appName: String = "") -> void:
+# !!!!! This is important !!!!!
+# This dictate the path of the home scene of the app and
+# its icon and logo.
+func setup( _appName: String = "") -> void:
 	appName = _appName
+	# This is the path of the home scene of the app
 	appScenePath = "res://"+appName+"/"+appName+".tscn"
+	# This is the path of the icon of the app
 	$Panel/icon.texture = load("res://assets/"+appName+"/icon.png")
+	# This is the path of the logo of the app
 	$Panel/button/logo.texture = load("res://assets/"+appName+"/logo.png")
 	self.visible = true
 
--- writeInGesture/shared/main_menu/main_menu.gd
+++ writeInGesture/shared/main_menu/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+# Main menu of the whole app
+
 var listSelectionRessource := load("res://shared/list_selection/list_selection.tscn")
 
 var appElementResource := load("res://shared/main_menu/app_element.tscn")
@@ -7,7 +9,7 @@ var appElementResource := load("res://shared/main_menu/app_element.tscn")
 func _ready():
 	for app in Global.apps:
 		var newApp = appElementResource.instance()
-		newApp.setUp(app)
+		newApp.setup(app)
 		$ScrollContainer/HBoxContainer.add_child_below_node($ScrollContainer/HBoxContainer/filler_start, newApp)
 
 func _on_list_change_pressed():
